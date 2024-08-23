13e3d5f9d5461a972198491da492daa33211a3cd
yo_ogino <ogy415@gmail.com>
2024-08-23 20:18:13 +0900

    fix bug

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ ln -s $DIR/.zshrc $HOME/.zshrc
 ln -s $DIR/.tmux.conf $HOME/.tmux.conf
 ln -s $DIR/.gitconfig $HOME/.gitconfig
 ln -s $DIR/Brewfile $HOME/Brewfile
-ln -s $DIR/.aerospace $HOME/.aerospace
+ln -s $DIR/.aerospace.toml $HOME/.aerospace.toml
 mkdir -p $HOME/.hammerspoon && ln -s $DIR/.hammerspoon/init.lua $HOME/.hammerspoon/init.lua
 mkdir -p $HOME/.config/skhd && ln -s $DIR/.config/skhd/skhdrc $HOME/.config/skhd/skhdrc
 
