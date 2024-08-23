0182294e825abe8f19ba0ef477a3678868b2a200
yo_ogino <ogy415@gmail.com>
2024-08-23 16:32:35 +0900

    warpの設定ファイル対応

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ ln -s $DIR/.gitconfig $HOME/.gitconfig
 ln -s $DIR/Brewfile $HOME/Brewfile
 ln -s $DIR/.aerospace $HOME/.aerospace
 mkdir -p $HOME/.hammerspoon && ln -s $DIR/.hammerspoon/init.lua $HOME/.hammerspoon/init.lua
+
+[ ! -d $HOME/.warp ] && cp -r $DIR/.warp $HOME/.warp
